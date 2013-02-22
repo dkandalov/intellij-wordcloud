e831339f064cf04769c562049fd947f5889ae89a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-02-22 08:58:52 +0000

    action to turn on/off tracking currently open file

--- plugin.groovy
+++ plugin.groovy
@@ -2,7 +2,6 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.actionSystem.PlatformDataKeys
-import com.intellij.openapi.actionSystem.Separator
 import com.intellij.openapi.fileEditor.FileEditorManagerAdapter
 import com.intellij.openapi.fileEditor.FileEditorManagerEvent
 import com.intellij.openapi.project.Project
@@ -11,9 +10,7 @@ import com.intellij.util.messages.MessageBusConnection
 import http.Util
 
 import static com.intellij.openapi.fileEditor.FileEditorManagerListener.FILE_EDITOR_MANAGER
-import static intellijeval.PluginUtil.currentFileIn
-import static intellijeval.PluginUtil.registerAction
-import static intellijeval.PluginUtil.show
+import static intellijeval.PluginUtil.*
 
 boolean trackCurrentFile = false
 String thisPluginPath = pluginPath
@@ -21,22 +18,35 @@ registerAction("WordCloud", "ctrl alt shift T") { AnActionEvent event ->
 	JBPopupFactory.instance.createActionGroupPopup(
 			"Word Cloud",
 			new DefaultActionGroup().with {
-				add(new AnAction("Show word cloud for selected files/packages") {
+				add(new AnAction("Show for selected files/packages") {
 					@Override void actionPerformed(AnActionEvent actionEvent) {
 						def files = PlatformDataKeys.VIRTUAL_FILE_ARRAY.getData(actionEvent.dataContext).toList()
 						WordCloud.showWordCloudFor(files, thisPluginPath)
 					}
 				})
-				add(new Separator())
-				add(new AnAction("Track opened file and build word cloud for it") {
-					@Override void actionPerformed(AnActionEvent e) {
-						// TODO
-					}
-				})
-				add(new AnAction("Show word cloud for currently open file") {
-					@Override void actionPerformed(AnActionEvent e) {
-						WordCloud.openCurrentEditorBrowser()
-					}
+				add(new DefaultActionGroup("Live Cloud", true).with {
+					add(new AnAction() {
+						@Override void actionPerformed(AnActionEvent e) {
+							trackCurrentFile = !trackCurrentFile
+							if (trackCurrentFile) {
+								restartEditorTracker(e.project, thisPluginPath)
+								show("Started")
+							} else {
+								stopEditorTracker()
+								show("Stopped")
+							}
+						}
+
+						@Override void update(AnActionEvent e) {
+							e.presentation.text = (trackCurrentFile ? "Stop tracking open file" : "Start tracking open file")
+						}
+					})
+					add(new AnAction("Show in browser") {
+						@Override void actionPerformed(AnActionEvent e) {
+							WordCloud.openCurrentEditorBrowser()
+						}
+					})
+					it
 				})
 				it
 			},
@@ -48,11 +58,18 @@ registerAction("WordCloud", "ctrl alt shift T") { AnActionEvent event ->
 show("Loaded WordCloud action<br/>Use Ctrl+Alt+Shift+T to open popup window")
 
 
-def project = event.project
-Util.changeGlobalVar("editorTracker") { oldTracker ->
-	oldTracker?.stop()
-	if (trackCurrentFile)
+
+def restartEditorTracker(Project project, String thisPluginPath) {
+	Util.changeGlobalVar("editorTracker") { oldTracker ->
+		oldTracker?.stop()
 		new EditorTracker(project, thisPluginPath).start()
+	}
+}
+
+def stopEditorTracker() {
+	Util.changeGlobalVar("editorTracker") { oldTracker ->
+		oldTracker?.stop()
+	}
 }
 
 class EditorTracker {
