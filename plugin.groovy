3ee07ead7284761fe98537e15c7bf96cc2cb2b2a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2013-02-23 13:48:14 +0000

    adding word cloud to Tools menu

--- plugin.groovy
+++ plugin.groovy
@@ -1,3 +1,4 @@
+import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DefaultActionGroup
@@ -12,50 +13,59 @@ import http.Util
 import static com.intellij.openapi.fileEditor.FileEditorManagerListener.FILE_EDITOR_MANAGER
 import static intellijeval.PluginUtil.*
 
+
 boolean trackCurrentFile = false
 String thisPluginPath = pluginPath
+
+def wordCloudActions = new DefaultActionGroup("Word Cloud", true).with {
+	add(new AnAction("Show for selected files/packages") {
+		@Override void actionPerformed(AnActionEvent actionEvent) {
+			def files = PlatformDataKeys.VIRTUAL_FILE_ARRAY.getData(actionEvent.dataContext).toList()
+			WordCloud.showWordCloudFor(files, thisPluginPath)
+		}
+	})
+	add(new DefaultActionGroup("Live Cloud", true).with {
+		add(new AnAction() {
+			@Override void actionPerformed(AnActionEvent event) {
+				trackCurrentFile = !trackCurrentFile
+				if (trackCurrentFile) {
+					restartEditorTracker(event.project, thisPluginPath)
+					show("Started")
+				} else {
+					stopEditorTracker()
+					show("Stopped")
+				}
+			}
+
+			@Override void update(AnActionEvent event) {
+				event.presentation.text = (trackCurrentFile ? "Stop tracking open file" : "Start tracking open file")
+			}
+		})
+		add(new AnAction("Show in browser") {
+			@Override void actionPerformed(AnActionEvent event) {
+				WordCloud.openCurrentEditorBrowser()
+			}
+
+			@Override void update(AnActionEvent event) {
+				event.presentation.enabled = trackCurrentFile
+			}
+		})
+		it
+	})
+	it
+}
+addToActionGroup("ToolsMenu", wordCloudActions)
+
 registerAction("WordCloud", "ctrl alt shift T") { AnActionEvent event ->
 	JBPopupFactory.instance.createActionGroupPopup(
 			"Word Cloud",
-			new DefaultActionGroup().with {
-				add(new AnAction("Show for selected files/packages") {
-					@Override void actionPerformed(AnActionEvent actionEvent) {
-						def files = PlatformDataKeys.VIRTUAL_FILE_ARRAY.getData(actionEvent.dataContext).toList()
-						WordCloud.showWordCloudFor(files, thisPluginPath)
-					}
-				})
-				add(new DefaultActionGroup("Live Cloud", true).with {
-					add(new AnAction() {
-						@Override void actionPerformed(AnActionEvent e) {
-							trackCurrentFile = !trackCurrentFile
-							if (trackCurrentFile) {
-								restartEditorTracker(e.project, thisPluginPath)
-								show("Started")
-							} else {
-								stopEditorTracker()
-								show("Stopped")
-							}
-						}
-
-						@Override void update(AnActionEvent e) {
-							e.presentation.text = (trackCurrentFile ? "Stop tracking open file" : "Start tracking open file")
-						}
-					})
-					add(new AnAction("Show in browser") {
-						@Override void actionPerformed(AnActionEvent e) {
-							WordCloud.openCurrentEditorBrowser()
-						}
-					})
-					it
-				})
-				it
-			},
+			wordCloudActions,
 			event.dataContext,
 			JBPopupFactory.ActionSelectionAid.SPEEDSEARCH,
 			true
 	).showCenteredInCurrentWindow(event.project)
 }
-show("Loaded WordCloud action<br/>Use Ctrl+Alt+Shift+T to open popup window")
+show("Loaded WordCloud action<br/>Use Ctrl+Alt+Shift+T to open popup window<br/>or Main Menu -> Tools -> Word Cloud")
 
 
 
@@ -100,5 +110,26 @@ class EditorTracker {
 		connection.disconnect()
 	}
 }
+
+static def addToActionGroup(String actionGroupId, AnAction action) {
+	def myId = ActionManager.instance.getId(action)
+	def actionGroup = findActionGroup(actionGroupId)
+	actionGroup.getChildren(null).findAll {
+		myId == ActionManager.instance.getId(it)
+	}.each {
+		actionGroup.remove(it)
+	}
+	actionGroup.add(action)
+}
+
+private static DefaultActionGroup findActionGroup(String actionGroupId) {
+	if (actionGroupId != null && actionGroupId) {
+		def action = ActionManager.instance.getAction(actionGroupId)
+		action instanceof DefaultActionGroup ? action : null
+	} else {
+		null
+	}
+}
+
 
 
